83a62a00c2b836eba186bf2cc0b9a2536ae1c66a
Philip Brown <pwbrown24@gmail.com>
2022-12-01 05:16:38 +0000

    completing day 1 of 2022

--- y2022/src/bin/d1_2022.rs
+++ y2022/src/bin/d1_2022.rs
@@ -3,4 +3,22 @@ use utils::get_input_data;
 #[tokio::main]
 async fn main() {
     let data = get_input_data("2022", "1").await.trim().to_string();
+
+    let mut elves_cals: Vec<i32> = vec![];
+
+    let mut current = 0;
+
+    for line in data.lines() {
+        if line.is_empty() {
+            elves_cals.push(current);
+            current = 0;
+        } else {
+            let cal: i32 = line.parse().unwrap();
+            current += cal;
+        }
+    }
+
+    elves_cals.sort();
+    println!("Part 1 Answer: {} calories", elves_cals.last().unwrap());
+    println!("Part 2 Answer: {} calories", elves_cals.iter().rev().take(3).sum::<i32>());
 }
